d2a30aee9752557eb975a180ebcda48fdf8817e0
mamentudicky <dickymamentu@gmail.com>
2024-11-20 20:00:44 +0800

    add component

--- src/config/Firebase/index.js
+++ src/config/Firebase/index.js
@@ -1,19 +1,19 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAkhucq6DnPCDi19vCQXdHGyvMiLi9CAUQ",
-  authDomain: "my-cv-de2af.firebaseapp.com",
-  projectId: "my-cv-de2af",
-  storageBucket: "my-cv-de2af.firebasestorage.app",
-  messagingSenderId: "1065735032423",
-  appId: "1:1065735032423:web:52e0d883b70638e1040ec2",
-  measurementId: "G-Y3HWBLQ9R0"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// Import the functions you need from the SDKs you need 
+import { initializeApp } from "firebase/app"; 
+// TODO: Add SDKs for Firebase products that you want to use 
+// https://firebase.google.com/docs/web/setup#available-libraries 
+ 
+// Your web app's Firebase configuration 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional 
+const firebaseConfig = { 
+  apiKey: "AIzaSyAkhucq6DnPCDi19vCQXdHGyvMiLi9CAUQ", 
+  authDomain: "my-cv-de2af.firebaseapp.com", 
+  projectId: "my-cv-de2af", 
+  storageBucket: "my-cv-de2af.firebasestorage.app", 
+  messagingSenderId: "1065735032423", 
+  appId: "1:1065735032423:web:52e0d883b70638e1040ec2", 
+  measurementId: "G-Y3HWBLQ9R0" 
+}; 
+ 
+// Initialize Firebase 
+const app = initializeApp(firebaseConfig); 
